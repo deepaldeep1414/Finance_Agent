b9bc3a4b749609c633ef8aebc683278b535698bf
Deepal Deep <deepaldeep.23@gmail.com>
2026-04-21 03:58:02 +0530

    Added build script for Render deployment

--- build.sh
+++ build.sh
@@ -2,16 +2,15 @@
 # exit on error
 set -o errexit
 
-# Install Backend Dependencies
+# Install Node.js dependencies
 echo "Installing Node.js dependencies..."
 cd backend
 npm install
-cd ..
 
-# Install Python Dependencies
+# Go back to root and install Python dependencies
 echo "Installing Python dependencies..."
-cd agent_v2
-pip install -r requirements.txt
 cd ..
+pip install -r agent_v2/requirements.txt
+pip install -r requirements.txt
 
 echo "Build Complete!"
